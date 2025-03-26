f8628ab77ba98fa86580b2ef561194b70aa8e1a9
ivapremosa <ivika_2000@hotmail.com>
2025-03-26 20:39:27 +0100

    Fix OrderService tests: configure MongoDB TestContainers

--- microservices/OrderService/src/test/java/com/example/demo/repository/OrderRepositoryTest.java
+++ microservices/OrderService/src/test/java/com/example/demo/repository/OrderRepositoryTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,7 +20,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
 @ActiveProfiles("test")
+@Testcontainers
 class OrderRepositoryTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Autowired
     private OrderRepository orderRepository;
