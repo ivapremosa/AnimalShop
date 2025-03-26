a28bfe65789b54c539189ea7b31f217eb9ff38c8
ivapremosa <ivika_2000@hotmail.com>
2025-03-26 20:43:24 +0100

    Simplify MongoDB test configuration and update container version

--- microservices/OrderService/src/test/java/com/example/demo/repository/OrderRepositoryTest.java
+++ microservices/OrderService/src/test/java/com/example/demo/repository/OrderRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -19,16 +18,16 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
-@ActiveProfiles("test")
 @Testcontainers
 class OrderRepositoryTest {
 
     @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.2");
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add("spring.data.mongodb.database", () -> "test");
     }
 
     @Autowired
